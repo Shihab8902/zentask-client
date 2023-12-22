1601a0e8a5997d6da36c4e6d35c1be3b94d57a0b
Shihab Hasan <shihabbd018@gmail.com>
2023-12-22 16:30:22 +0600

    configure for production deploy

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:9000"
+    baseURL: "https://zentask-steel.vercel.app"
 });
 
 const useAxios = () => {
